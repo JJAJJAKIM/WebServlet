fd7ba1864a38b105ba0cf4a46a9380d4e8de9558
JJAJJAKIM <csm9121@gmail.com>
2024-06-05 14:53:56 +0900

    filter 사용 예제 1

--- WebServlet/WebContent/www/jsp/Form.jsp
+++ WebServlet/WebContent/www/jsp/Form.jsp
@@ -5,12 +5,13 @@
 <meta charset="UTF-8">
 <title>JSP 화면</title>
 <link rel="icon" href="<%=request.getContextPath()%>/www/img/logo.png" type="image/x-icon">
-<link href="<%=request.getContextPath()%>/www/lib/bootstrap@5.2.3/css/bootstrap.min.css" rel="stylesheet">
+<%-- <link href="<%=request.getContextPath()%>/www/lib/bootstrap@5.2.3/css/bootstrap.min.css" rel="stylesheet"> --%>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
 <div class="container mt-3">
 	<h1 class="text-center">데이터를 입력하세요</h1>
-	<form action="/WebServlet/Result" method="post"> <!-- form태그는 데이터를 전송하는 영역. action이 Java파일을 호출하는 부분 -->
+	<form action="/Result" method="post"> <!-- form태그는 데이터를 전송하는 영역. action이 Java파일을 호출하는 부분 -->
 		<div class="mb-3 mt-3">
 			<label for="cnt" class="form-label">회전수 :</label>
 			<input type="number" class="form-control" id="cnt" name="cnt" value="0"> <!-- name이 데이터와 관련된 키이다. id는 style과 관련된것 -->
--- WebServlet/src/Result.java
+++ WebServlet/src/Result.java
@@ -3,6 +3,7 @@
 import java.io.IOException;
 import java.io.PrintStream;
 import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
@@ -16,24 +17,23 @@ public class Result extends HttpServlet {
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		response.setCharacterEncoding("UTF-8"); // charset 과 contenttype 설정은 상단부분에서 처리해야한다.
-		response.setContentType("text/html; charset=UTF-8");
-
+//		response.setCharacterEncoding("UTF-8"); // 1.charset 과 contenttype 설정은 상단부분에서 처리해야한다.
+//		response.setContentType("text/html; charset=UTF-8");
+//		Util.ko(request, response); 4.따로 인코딩과 관련된 메소드를 만들어 호출
 		int cnt = Integer.parseInt(request.getParameter("cnt"));
 		String txt = request.getParameter("txt");
 		PrintWriter pw =  response.getWriter();
 		for(int i=0; i < cnt ; i++) {
 			pw.append((i+1)+","+ txt + "</br>");
-		
 		}
-		
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		request.setCharacterEncoding("UTF-8"); // post방식은 받아오는 데이터(request)도 setCharacterEncoding을 써야한다. 데이터가 body에 담겨오기 때문이다.
-		response.setCharacterEncoding("UTF-8");
-		response.setContentType("text/html; charset=UTF-8");
+//		request.setCharacterEncoding("UTF-8"); // 2.post방식은 받아오는 데이터(request)도 setCharacterEncoding을 써야한다. 데이터가 body에 담겨오기 때문이다.
+//		response.setCharacterEncoding("UTF-8");
+//		response.setContentType("text/html; charset=UTF-8");
+//		Util.ko(request, response);
 		int cnt = Integer.parseInt(request.getParameter("cnt"));
 		String txt = request.getParameter("txt");
 		PrintWriter pw =  response.getWriter();
@@ -41,5 +41,11 @@ public class Result extends HttpServlet {
 			pw.append((i+1)+","+ txt + "</br>");
 		}
 	}
-
+///////////3. ko라는 메소드를 따로 분리하여 클래스 생성후 static를 사용하여 메모리를 항상 할당한다면 매번 인코딩해야할 필요가 없어진다.	
+//	public void ko(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		request.setCharacterEncoding("UTF-8"); 
+//		response.setCharacterEncoding("UTF-8");
+//		response.setContentType("text/html; charset=UTF-8");
+//		
+//	}
 }
